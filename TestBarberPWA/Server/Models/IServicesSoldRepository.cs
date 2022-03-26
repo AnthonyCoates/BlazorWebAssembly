ceaed8fb974086196effd79bcad006ff840d46c5
Anthony Coates <anthjcoates@gmail.com>
2022-03-26 14:10:09 +0000

    Implemented a services sold interface and repository

---
+++ TestBarberPWA/Server/Models/IServicesSoldRepository.cs
@@ -0,0 +1,13 @@
+﻿using TestBarberPWA.Shared;
+
+namespace TestBarberPWA.Server.Models
+{
+    public interface IServicesSoldRepository
+    {
+        Task<IEnumerable<ServiceSold>> GetServicesSold();
+        Task<IEnumerable<ServiceSold>> GetServicesSoldByAppointment(int appointmentID);
+        Task<IEnumerable<ServiceSold>> GetServicesSoldByService(int serviceID);
+
+        Task<ServiceSold> GetServiceSold(int appointmentID, int serviceID);
+    }
+}
---
+++ TestBarberPWA/Server/Models/ServicesSoldRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestBarberPWA.Shared;
+
+namespace TestBarberPWA.Server.Models
+{
+    public class ServicesSoldRepository : IServicesSoldRepository
+    {
+        private readonly AppDBContext appDBContext;
+
+        public ServicesSoldRepository(AppDBContext appDBContext)
+        {
+            this.appDBContext = appDBContext;
+        }
+
+        public async Task<ServiceSold> GetServiceSold(int appointmentID, int serviceID)
+        {
+            return await appDBContext.ServicesSold
+                .FirstOrDefaultAsync(a => a.AppointmentID == appointmentID && a.ServiceID == serviceID);
+        }
+
+        public async Task<IEnumerable<ServiceSold>> GetServicesSold()
+        {
+            return await appDBContext.ServicesSold.ToListAsync();
+        }
+
+        public async Task<IEnumerable<ServiceSold>> GetServicesSoldByAppointment(int appointmentID)
+        {
+            IQueryable<ServiceSold> query = appDBContext.ServicesSold;
+
+            return await query.Where(a => a.AppointmentID == appointmentID).ToListAsync();
+        }
+
+        public async Task<IEnumerable<ServiceSold>> GetServicesSoldByService(int serviceID)
+        {
+            IQueryable<ServiceSold> query = appDBContext.ServicesSold;
+
+            return await query.Where(a => a.ServiceID == serviceID).ToListAsync();
+        }
+    }
+}
